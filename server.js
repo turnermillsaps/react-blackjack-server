28e8ba9ee34adef26edce4cdbf453a03c7487a59
Turner Millsaps <turnermillsaps.plp@gmail.com>
2020-07-18 14:20:20 -0400

    Added separate call to get game data for the user

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const db = require('./models');
 const PORT = process.env.PORT || 3001;
@@ -8,6 +9,19 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Uncomment to set up whitelist for localhost testing
+/* const whitelist = ['http://localhost:3000']
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+app.use(cors(corsOptions)) */
+
 // Routes
 // ** Test express server
 app.get("/", (req, res) => {
@@ -15,7 +29,8 @@ app.get("/", (req, res) => {
 })
 
 // ** Create user if it does not already exist, otherwise return user data
-app.get("/api/findOrCreateUser", (req, res) => {
+app.post("/api/findOrCreateUser", (req, res) => {
+    console.log(`Request body: ${JSON.stringify(req.body)}`)
     if (!req.body.email || !req.body.name || !req.body.imageUrl || !req.body.googleId) {
         res.json({ ...req.body, error: `Request does not contain all parameters needed.`})
     } else {
@@ -27,12 +42,26 @@ app.get("/api/findOrCreateUser", (req, res) => {
                 imageUrl: req.body.imageUrl,
                 googleId: req.body.googleId
             }
-        }).then(result => { res.json(result) })
-          .catch(err => {
+        }).then(result => { 
+            res.json(result)
+        }).catch(err => {
               console.error(err)
               res.send('An error occurred while attempting to find or create user.')
           })
-    }
+    } 
+})
+
+// ** Get game data from the db
+app.get("/api/getUserGameData/:id", (req, res) => {
+    console.log(JSON.stringify(req.params))
+    db.sequelize.query(`SELECT SUM(money_won_loss), COUNT(id) FROM user_games WHERE user_id = ${req.params.id}`)
+        .then(result => {
+            console.log(`Game Data: ${JSON.stringify(result[0][0])}`)
+            res.json(result[0][0]);
+        }).catch(err => { 
+            console.error(err)
+            res.send(err)
+        })
 })
 
 // ** Post game data to db
